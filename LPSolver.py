9ca9a95f5e237e4ab7f4e14af8369f069f3d4ed9
J0eppp <joep.van.dijk04@gmail.com>
2021-09-27 16:48:53 +0200

    Adding variables

--- LPSolver.py
+++ LPSolver.py
@@ -1,7 +1,9 @@
 import mip
+from mip import Model, xsum, minimize, INTEGER
 import utils
 from time import sleep
 import datatypes as types
+from math import floor
 
 from Solver import Solver
 
@@ -9,15 +11,43 @@ class LPSolver(Solver):
     """
     The lineair programming solution class of our problem
     """
-    def __init__(self, timetable: types.Timetable):
+    def __init__(self, timetable: types.Timetable, amount_of_hours_a_day: int, amount_of_days_a_week: int):
         super().__init__(timetable)
         self.model = None
-        # self.timetable = timetable
-    
+        self.amount_of_hours_a_day = amount_of_hours_a_day
+        self.amount_of_days_a_week = amount_of_days_a_week
+
     def solve(self):
         return self.__solve(self.timetable)
     
+    def __calc_hour(self, hour) -> int:
+        return round(((hour / self.amount_of_hours_a_day) - floor(hour / self.amount_of_hours_a_day)) * self.amount_of_hours_a_day)
+    
+    def __calc_day(self, hour) -> int:
+        return floor(hour / self.amount_of_hours_a_day)
+
     def __solve(self, timetable: types.Timetable):
-        self.model = mip.Model(sense=mip.MAXIMIZE, solver_name=mip.CBC)
+        self.model = Model()
         
-        return timetable
+        # Variables
+        H = self.amount_of_days_a_week * self.amount_of_hours_a_day
+
+        utils.uprint("Creating variables")
+        start_time = time.process_time()
+
+        S = []
+        
+
+        end_time = time.process_time()
+        utils.uprint("Done creating variables")
+        utils.urpint(f"Creating variables took: {end_time - start_time} seconds")
+
+        # Constrains
+
+
+        # Objective function
+        self.model.objective = minimize(self.timetable.countGapHours())
+
+        self.model.optimize()
+
+        return self.model
--- datatypes.py
+++ datatypes.py
@@ -37,6 +37,7 @@ class Timetable:
     lessons: List[Lesson]
     
     def countGapHours(self) -> int:
+        """Couont all the gap hours in the timetable"""
         timetable = [[[]]]
         
         gap_hours = 0
@@ -56,3 +57,13 @@ class Timetable:
                     gap_hours += amount
         
         return gap_hours
+    
+    def canSchedule(self, d, h, t, g) -> bool:
+        """Check if this lesson is allowed to be scheduled"""
+        for lesson in self.lessons:
+            ds, hs, ts, gs = lesson.day, lesson.hour, lesson.teacher, lesson.group
+
+            if (ds == d and hs == h) and (gs == g or ts == t):
+                return False
+        
+        return True
