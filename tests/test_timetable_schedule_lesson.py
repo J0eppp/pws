dc783d2708fc4aee6188bfa50a3418f936bdea9e
J0eppp <joep.van.dijk04@gmail.com>
2021-10-05 18:05:32 +0200

    Write two tests

--- tests/test_timetable_schedule_lesson.py
+++ tests/test_timetable_schedule_lesson.py
@@ -1,15 +1,34 @@
-import solver
 from solver.parser import parse_json_file
 from solver.datatypes import Timetable
 
 
-def test_timetable_schedule_lesson():
+def test_timetable_schedule_lesson_1():
+    # Test 1
     timetable: Timetable = parse_json_file(
         "tests/test0.json")
-    print(timetable)
     assert len(timetable.groups) == 1
     assert len(timetable.teachers) == 1
     assert timetable.create_feasible_timetable() == True
     assert len(timetable.lessons) == 1
     assert len(timetable.groups[0].lessons) == 1
     assert len(timetable.teachers[0].lessons) == 1
+
+
+def test_timetable_schedule_lesson_2():
+    # Test 2
+    timetable: Timetable = parse_json_file("tests/test1.json")
+    assert len(timetable.groups) == 3
+    assert len(timetable.teachers) == 3
+    assert timetable.create_feasible_timetable() == True
+    assert len(timetable.lessons) == 33
+    for group in timetable.groups:
+        assert len(group.lessons) == 11
+
+    subject_info = {}
+
+    for si in timetable.subject_information:
+        subject_info[si.subject] = si.amount
+
+    for teacher in timetable.teachers:
+        assert len(
+            teacher.lessons) == subject_info[teacher.subject] * len(timetable.groups)
